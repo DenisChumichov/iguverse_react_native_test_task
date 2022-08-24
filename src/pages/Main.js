b6d402af9fbc7910d2eca3402c4912473af5f87b
Denis <chumichov16@gmail.com>
2022-08-25 01:52:08 +0300

    feature/load-image

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,68 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {
+  BackHandler,
+  ImageBackground,
+  StatusBar,
+  StyleSheet,
+  View,
+} from 'react-native';
+import {launchImageLibrary} from 'react-native-image-picker';
+import Icon from 'react-native-vector-icons/AntDesign';
+import IconButton from '../components/commons/IconButton';
 
 const Main = () => {
-  return <></>;
+  const [chosenImageUri, setChosenImageUri] = useState();
+
+  const onChooseImage = async () => {
+    await launchImageLibrary({}, response => {
+      if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else {
+        if (!response.didCancel) {
+          setChosenImageUri(response.assets[0].uri);
+        }
+      }
+    });
+  };
+
+  return (
+    <>
+      <StatusBar translucent backgroundColor={'transparent'} />
+      {chosenImageUri && (
+        <ImageBackground
+          style={styles.backgroundImage}
+          resizeMode="cover"
+          source={{uri: chosenImageUri}}
+        />
+      )}
+
+      <View style={styles.header}>
+        <IconButton
+          onPress={() => BackHandler.exitApp()}
+          icon={<Icon name="arrowleft" size={25} />}
+        />
+        <IconButton
+          onPress={onChooseImage}
+          icon={<Icon name="upload" size={25} />}
+        />
+      </View>
+    </>
+  );
 };
+
+const styles = StyleSheet.create({
+  backgroundImage: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  header: {
+    position: 'absolute',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    padding: 25,
+  },
+});
 
 export default Main;
